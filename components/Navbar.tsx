2b1edafc553b8907a4548b500ea506afbfa09ebb
chiragbidx <chirag@bidx.ai>
2026-03-23 15:30:15 +0530

    Update navbar to new content contract

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,10 @@ import { getContent } from "@/content/content";
 
 export const Navbar = () => {
   const { navbar } = getContent();
-
   return (
     <header className="px-10 pt-8">
       <span className="text-[22px] font-bold tracking-tight">
-        {navbar.brandPrefix}
-        <span className="text-[15px]">{navbar.brandSuffix}</span>
+        {navbar.brandLabel}
       </span>
     </header>
   );
